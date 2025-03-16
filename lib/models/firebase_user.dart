8786ca4f8c49435bc119d1bc7e039103660e9abc
anjoserohijos-programming <aserohijos88@gmail.com>
2025-03-17 00:57:03 +0800

    [add]: initial data model for FirebaseUser, this class only has the essential information from the user, and the specific attributes for Driver and Passenger are divided to their own appropriate classes

---
+++ lib/models/firebase_user.dart
@@ -0,0 +1,18 @@
+class FirebaseUserDetails {
+  String _firstName;
+  String _middleName;
+  String _lastName;
+  String _sexAtBirth;
+  String _birthDate;
+
+  FirebaseUserDetails({required firstName, required middleName, required lastName, required sexAtBirth, required birthDate})
+      : _firstName = firstName,
+        _middleName = middleName,
+        _lastName = lastName,
+        _sexAtBirth = sexAtBirth,
+        _birthDate = birthDate;
+
+
+  //getters
+  
+}
