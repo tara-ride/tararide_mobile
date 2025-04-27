dad0646c91a89aeb32ef94db94f8fbbe1c5e31a3
anjoserohijos-programming <aserohijos88@gmail.com>
2025-04-28 01:53:24 +0800

    [add]: completed the upload steps for firebase

--- lib/bloc/user_sign_up/user_sign_up_bloc.dart
+++ lib/bloc/user_sign_up/user_sign_up_bloc.dart
@@ -1,5 +1,8 @@
 import 'package:bloc/bloc.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:equatable/equatable.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:tararide_mobile/models/passenger.dart';
 
 part 'user_sign_up_event.dart';
@@ -15,11 +18,54 @@ class UserSignUpBloc extends Bloc<UserSignUpEvent, UserSignUpState> {
     on<SignUpUser>(
       (event, emit) async {
         try {
+          FirebaseAuth firebaseAuth = FirebaseAuth.instance;
+
           // Call the API to sign up the user
           // If the API call is successful, emit UserSignUpSuccess
           // If the API call is unsuccessful, emit UserSignUpFailure
           emit(UserSignUpLoading());
-          //await Future.delayed(const Duration(seconds: 7));
+          await Future.delayed(const Duration(seconds: 1));
+          UserCredential? firebaseUserCredential = await firebaseAuth.createUserWithEmailAndPassword(email: event.email, password: event.password);
+          if (firebaseUserCredential.user != null) {
+            //emit(UserSignUpSuccess());
+            //process account information
+            //process personal information
+            //process contact information
+
+            FirebaseFirestore firebaseFirestore = FirebaseFirestore.instance;
+            FirebaseStorage firebaseStorage = FirebaseStorage.instance;
+            await firebaseFirestore.collection("account_information").doc(firebaseUserCredential.user!.uid).set({
+              "business_role": "passenger",
+              "created_by": "tararide_automated_service",
+              "created_on": DateTime.now(),
+              "email_address": firebaseUserCredential.user!.email,
+              "uuid": firebaseUserCredential.user!.uid,
+            });
+            DocumentSnapshot<Map<String, dynamic>> accountData = await firebaseFirestore.collection("account_information").doc(firebaseUserCredential.user!.uid).get();
+
+            await firebaseFirestore.collection("personal_information").doc(firebaseUserCredential.user!.uid).set({
+              "first_name": event.firstName,
+              "middle_name": event.middleName!,
+              "last_name": event.lastName,
+              "sex_at_birth": event.sexAtBirth,
+              "birth_date": event.birthDate,
+            });
+            DocumentSnapshot<Map<String, dynamic>> personalData = await firebaseFirestore.collection("personal_information").doc(firebaseUserCredential.user!.uid).get();
+
+            await firebaseFirestore.collection("contact_information").doc(firebaseUserCredential.user!.uid).set({
+              "contact_no": event.contactNo,
+              "home_address": event.homeAddress,
+            });
+            DocumentSnapshot<Map<String, dynamic>> contactData = await firebaseFirestore.collection("contact_information").doc(firebaseUserCredential.user!.uid).get();
+
+            if (accountData.exists && personalData.exists && contactData.exists) {
+              emit(UserSignUpSuccess());
+            } else {
+              emit(const UserSignUpError("Unable to upload user data."));
+            }
+          } else {
+            emit(const UserSignUpError("Unable to fetch user data."));
+          }
           //emit(UserSignUpSuccess());
           //emit(UserSignUpFailure());
         } catch (e) {
