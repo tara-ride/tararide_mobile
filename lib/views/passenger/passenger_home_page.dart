0b055734abd31f894cb1e87d07422cd23f125c50
anjoserohijos-programming <aserohijos88@gmail.com>
2025-04-28 01:54:13 +0800

    [add]: added passenger home page placeholder UI

--- lib/views/passenger/passenger_home_page.dart
+++ lib/views/passenger/passenger_home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
 
 class PassengerHomePage extends StatefulWidget {
   const PassengerHomePage({super.key, required this.title});
@@ -10,11 +11,130 @@ class PassengerHomePage extends StatefulWidget {
 }
 
 class _PassengerHomePageState extends State<PassengerHomePage> {
+  int pageIndex = 0;
+  final List<Widget> _widgetOptions = <Widget>[
+    Container(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          SizedBox(
+            height: 300,
+            child: LottieBuilder.asset("assets/under_construction.json"),
+          ),
+          const Text(
+            "Home",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          const Text("This area is still under construction."),
+          const Text("We'll get there soon!"),
+        ],
+      ),
+    ),
+    Container(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          SizedBox(
+            height: 300,
+            child: LottieBuilder.asset("assets/under_construction.json"),
+          ),
+          const Text(
+            "Chat",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          const Text("This area is still under construction."),
+          const Text("We'll get there soon!"),
+        ],
+      ),
+    ),
+    Container(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          SizedBox(
+            height: 300,
+            child: LottieBuilder.asset("assets/under_construction.json"),
+          ),
+          const Text(
+            "History",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          const Text("This area is still under construction."),
+          const Text("We'll get there soon!"),
+        ],
+      ),
+    ),
+    Container(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          SizedBox(
+            height: 300,
+            child: LottieBuilder.asset("assets/under_construction.json"),
+          ),
+          const Text(
+            "Profile",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          const Text("This area is still under construction."),
+          const Text("We'll get there soon!"),
+        ],
+      ),
+    ),
+  ];
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Center(
-        child: Text("Passenger Main APp, initializations happen here."),
+        child: _widgetOptions.elementAt(pageIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: (value) {
+          print("Value $value");
+
+          setState(() {
+            pageIndex = value;
+          });
+          print("$pageIndex");
+        },
+        backgroundColor: const Color.fromARGB(255, 93, 47, 123),
+        type: BottomNavigationBarType.shifting,
+        selectedItemColor: const Color.fromARGB(255, 93, 47, 123),
+        unselectedItemColor: const Color.fromARGB(255, 119, 107, 128),
+        elevation: 14,
+        currentIndex: pageIndex,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: "Home",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.chat),
+            label: "Chat",
+          ),
+          BottomNavigationBarItem(icon: Icon(Icons.history), label: "History"),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: "Profile"),
+        ],
       ),
     );
   }
