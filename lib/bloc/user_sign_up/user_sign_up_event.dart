5da4a2f7be057981975dad26120d45a049f231b5
anjoserohijos-programming <aserohijos88@gmail.com>
2025-04-28 01:53:40 +0800

    [add]: added event for user sign up

--- lib/bloc/user_sign_up/user_sign_up_event.dart
+++ lib/bloc/user_sign_up/user_sign_up_event.dart
@@ -9,10 +9,23 @@ sealed class UserSignUpEvent extends Equatable {
 
 class SignUpAwaiting extends UserSignUpEvent {}
 
-class SignUpUser extends UserSignUpEvent {
+final class SignUpUser extends UserSignUpEvent {
+  //account_information
   final String email;
   final String password;
-  const SignUpUser(this.email, this.password);
 
-  List<Object> get props => [email, password];
+  //personal_information
+  final String firstName;
+  final String? middleName;
+  final String lastName;
+  final String sexAtBirth;
+  final DateTime birthDate;
+
+  //contact_information
+  final String contactNo;
+  final String homeAddress;
+
+  const SignUpUser({required this.email, required this.password, required this.firstName, required this.middleName, required this.lastName, required this.sexAtBirth, required this.birthDate, required this.contactNo, required this.homeAddress});
+
+  List<Object> get props => [email, password, firstName, middleName!, lastName, sexAtBirth, birthDate, contactNo, homeAddress];
 }
